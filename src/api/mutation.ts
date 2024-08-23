8b4c73c4c81cd741f4e57ed07ba20cebb3747ef6
Youngmentor <olaleresuliton43@gmail.com>
2024-08-23 11:20:19 +0100

    created an env file and also added the login api in the mutataion file

--- src/api/mutation.ts
+++ src/api/mutation.ts
@@ -1,1 +1,8 @@
-{}
+import axios from "axios";
+
+const { VITE_ENDPOINT } = import.meta.env;
+
+export const adminLogin = async (data: any) => {
+    return await axios.post(`${VITE_ENDPOINT}/admin/login`, data)
+};
+

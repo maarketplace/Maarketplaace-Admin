ca9a9ce8f7bc6f11106fbddf22c02c2a5bd091e9
Youngmentor <olaleresuliton43@gmail.com>
2024-10-21 00:00:22 +0100

    updated the onError block to catch the error when the token expire

--- src/welcome/index.tsx
+++ src/welcome/index.tsx
@@ -5,12 +5,12 @@ const Welcome = () => {
     return (
         <div className='w-[100%] h-[100vh] flex items-center justify-center flex-col '>
             <img src="/MARKET.svg" alt="" className='w-[80px] mb-[50px]' />
-            <div className='w-[400px] h-[350px] bg-white shadow-[2px_2px_8px_2px_rgba(0,0,0,0.1)] flex justify-center flex-col items-center p-[10px] gap-[20px]'>
+            <div className='w-[400px] max-[650px]:w-[95%] h-[380px] bg-white shadow-[2px_2px_8px_2px_rgba(0,0,0,0.1)] flex justify-center flex-col items-center p-[10px] gap-[20px]'>
                 <img src="/login.png" alt="" className='w-[150px]' />
                 <span className='w-[95%] flex flex-col gap-[10px] items-center'>
                     <p className='text-[20px] mt-[20px]'>Welcome back</p>
                     <p className='text-center font-light text-[12px] '>Welcome to maarketplaace internal dashbaord - the back office for managing merchants, product, users and transaction.</p>
-                    <button className='w-[100%] h-[30px] bg-[#FFC300]' onClick={()=> navigate('/login')}>Login</button>
+                    <button className='w-[100%] h-[30px] bg-[#FFC300] mt-[20px]' onClick={()=> navigate('/login')}>Login</button>
                 </span>
             </div>
         </div>
